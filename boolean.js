ae2656c025d6ad711f2db042ad14d5f3cf05e2d5
cleonilson08 <cleonilson08@outlook.com>
2022-04-04 12:41:55 -0300

    exemplos aula02

---
+++ boolean.js
@@ -0,0 +1,13 @@
+// boolean 
+  // true - verdadeiro
+  //  false - falso
+
+  const primeironumero = 5;
+  const segundonumero = 5; 
+  let cadastroativo = true;
+  
+  //console.log(primeironumero === segundonumero)
+  
+  const texto1 ="Alura";
+  const texto2 ="alura";
+  console.log(texto1 === texto2)
---
+++ string.js
@@ -0,0 +1,12 @@
+const citacao = "meu nome é";
+const meunome = "cleonilson";
+
+console.log(citacao)
+
+//concatenação (+)
+
+console.log(citacao + meunome)
+
+
+
+//template string e template literal
