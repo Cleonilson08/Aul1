8a33353e9dd3234cdc7c84a8b5f45d05f29cc306
cleonilson08 <cleonilson08@outlook.com>
2022-04-04 11:35:14 -0300

    exemplos aula1

---
+++ aula01.js
@@ -0,0 +1,23 @@
+//typi numero
+
+const Numero = 3;
+
+const PrimeiroNumero = 3;
+const  SegundoNumero = 2;
+const SomaNumeros = PrimeiroNumero + SegundoNumero;
+console.log(SomaNumeros)
+
+
+//ponto flutaante
+
+const pontoFlutuante = 3.3;
+const numeroFlutuante = .5;
+
+const Operacacoaflutuante = PrimeiroNumero / pontoFlutuante;
+console.log(Operacacoaflutuante)
+
+
+// NaN -> Not a number (isso nâo é um número);
+
+const alura = "alura";
+console.log( alura * PrimeiroNumero)
